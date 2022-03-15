98df7df266dbf4cc9862736f5eb7e844b944977b
syedf <syedf@10.1.10.53>
2022-03-15 15:42:07 -0700

    I am uploading project

---
+++ src/Training/Hello.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Welcome to the world of JAVA ");
+		
+		System.out.println(50+70);
+
+	}
+
+}
